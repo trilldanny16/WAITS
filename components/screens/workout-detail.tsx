4ddc049582197e9f8e060122527e33fac56f5168
trilldanny16 <trilldill02@gmail.com>
2026-08-12 13:04:38 -0400

    Restore established workout detail states

--- components/screens/workout-detail.tsx
+++ components/screens/workout-detail.tsx
@@ -20,7 +20,6 @@ import { Avatar } from '../avatar'
 import { WorkoutTypeIcon } from '../workout-type-icon'
 import { formatTime, formatDateLabel } from '@/lib/date-utils'
 import { cn } from '@/lib/utils'
-import { isPersistedWorkoutId } from '@/lib/workout-identity'
 
 function getDirectionsUrl(lat: number, lng: number) {
   const destination = `${lat},${lng}`
@@ -56,8 +55,7 @@ export function WorkoutDetail({ id }: { id: string }) {
   const joined = hasJoined(workout)
   const full = isFull(workout)
   const spotsLeft = workout.maxParticipants - workout.attendees.length
-  const persistedWorkout = isPersistedWorkoutId(workout.id)
-  const canChat = persistedWorkout && (joined || isHost)
+  const canChat = joined || isHost
 
   return (
     <div className="flex h-full flex-col bg-background">
