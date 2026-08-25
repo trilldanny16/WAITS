1fdac594d706f53209e4f97e34e9eda798c2641a
trilldanny16 <trilldill02@gmail.com>
2026-08-25 10:10:47 -0400

    Add WAITS mobile foundation: src/app/(tabs)/index.tsx

---
+++ mobile/src/app/(tabs)/index.tsx
@@ -0,0 +1,3 @@
+import { PlaceholderScreen } from '@/components/PlaceholderScreen'
+export default function Feed() { return <PlaceholderScreen title="Ready to move" description="Your native WAITS feed will live here." /> }
+
