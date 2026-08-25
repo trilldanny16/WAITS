4311f9e59658dbb2a37dddcce9d8a797499a0f50
trilldanny16 <trilldill02@gmail.com>
2026-08-25 10:35:53 -0400

    Add native auth checkpoint: src/app/_layout.tsx

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import { activateAdapty } from '@/lib/adapty'
+import { AuthProvider } from '@/providers/AuthProvider'
 
 export default function RootLayout() {
   useEffect(() => {
@@ -9,10 +10,10 @@ export default function RootLayout() {
   }, [])
 
   return (
-    <>
+    <AuthProvider>
       <StatusBar style="dark" />
       <Stack screenOptions={{ headerShown: false }} />
-    </>
+    </AuthProvider>
   )
 }
 
