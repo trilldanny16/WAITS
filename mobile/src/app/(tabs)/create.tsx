717418c31b55144fa4e222837efdf40a452c59f8
trilldanny16 <trilldill02@gmail.com>
2026-08-25 10:11:04 -0400

    Add WAITS mobile foundation: src/app/(tabs)/create.tsx

---
+++ mobile/src/app/(tabs)/create.tsx
@@ -0,0 +1,3 @@
+import { PlaceholderScreen } from '@/components/PlaceholderScreen'
+export default function Create() { return <PlaceholderScreen title="New Workout" description="Host a workout and invite your followers." /> }
+
