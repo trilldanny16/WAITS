4db38bcd15456f359c3a1cd29c1add8296d61f21
trilldanny16 <trilldill02@gmail.com>
2026-08-25 10:10:55 -0400

    Add WAITS mobile foundation: src/app/(tabs)/search.tsx

---
+++ mobile/src/app/(tabs)/search.tsx
@@ -0,0 +1,3 @@
+import { PlaceholderScreen } from '@/components/PlaceholderScreen'
+export default function Search() { return <PlaceholderScreen title="Social" description="Search for friends, workouts, and gyms." /> }
+
