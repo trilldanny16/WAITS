af308921eb0bccf5a5cfbb6b61784d2390c0f2ac
trilldanny16 <trilldill02@gmail.com>
2026-08-25 10:10:07 -0400

    Add WAITS mobile foundation: src/app/_layout.tsx

---
+++ mobile/src/app/_layout.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react'
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import { activateAdapty } from '@/lib/adapty'
+
+export default function RootLayout() {
+  useEffect(() => {
+    void activateAdapty()
+  }, [])
+
+  return (
+    <>
+      <StatusBar style="dark" />
+      <Stack screenOptions={{ headerShown: false }} />
+    </>
+  )
+}
+
