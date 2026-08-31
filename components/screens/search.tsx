5dd041e92646ff8e61c6c304bd9099f08db47cc5
trilldanny16 <trilldill02@gmail.com>
2026-08-31 10:43:04 -0400

    Use workout date for Social join labels instead of a blanket override

--- components/screens/search.tsx
+++ components/screens/search.tsx
@@ -276,7 +276,7 @@ const realMatchedUsers = useMemo(() => {
             ) : (
               <div className="space-y-3">
                 {matchedWorkouts.map((w) => (
-                  <WorkoutCard key={w.id} workout={w} joinLabel="Lock Me In!" />
+                  <WorkoutCard key={w.id} workout={w} />
                 ))}
               </div>
             )}
